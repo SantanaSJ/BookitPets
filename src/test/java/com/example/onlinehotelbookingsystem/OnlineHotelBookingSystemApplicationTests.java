756c03c2fa7aff41a3ff3a016c29900dece2c272
Santanasj <slv_sntn@yahoo.com>
2022-04-30 18:11:42 +0300

    first commit

--- src/test/java/com/example/onlinehotelbookingsystem/OnlineHotelBookingSystemApplicationTests.java
+++ src/test/java/com/example/onlinehotelbookingsystem/OnlineHotelBookingSystemApplicationTests.java
@@ -1,13 +1,13 @@
-//package com.example.onlinehotelbookingsystem;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class OnlineHotelBookingSystemApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//
-//}
+package com.example.onlinehotelbookingsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineHotelBookingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
