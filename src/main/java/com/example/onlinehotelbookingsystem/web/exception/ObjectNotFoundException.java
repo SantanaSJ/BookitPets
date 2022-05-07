3383fba00f4d7a509f94529f94acd39aa408ac93
Santanasj <slv_sntn@yahoo.com>
2022-05-07 16:45:03 +0300

    first commit

--- src/main/java/com/example/onlinehotelbookingsystem/web/exception/ObjectNotFoundException.java
+++ src/main/java/com/example/onlinehotelbookingsystem/web/exception/ObjectNotFoundException.java
@@ -1,11 +1,11 @@
-//package com.example.onlinehotelbookingsystem.web.exception;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.web.bind.annotation.ResponseStatus;
-//
-//@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not found!")
-//public class ObjectNotFoundException extends RuntimeException {
-//    public ObjectNotFoundException(String message) {
-//        super(message);
-//    }
-//}
+package com.example.onlinehotelbookingsystem.web.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Resource not found!")
+public class ObjectNotFoundException extends RuntimeException {
+    public ObjectNotFoundException(String message) {
+        super(message);
+    }
+}
