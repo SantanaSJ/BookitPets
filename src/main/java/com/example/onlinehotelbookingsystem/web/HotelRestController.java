a2461f9056eb0ae9d76320f31b14430f14ab0e3b
Santanasj <slv_sntn@yahoo.com>
2022-05-07 13:42:04 +0300

    first commit

--- src/main/java/com/example/onlinehotelbookingsystem/web/HotelRestController.java
+++ src/main/java/com/example/onlinehotelbookingsystem/web/HotelRestController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(allowedHeaders = "*")
+@CrossOrigin("http://localhost:8080/accommodations")
 @RestController
 public class HotelRestController {
 
