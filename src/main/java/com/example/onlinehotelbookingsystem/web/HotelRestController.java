2877758bc1124bbe8bf5ae7ad302525afc0bee18
Santanasj <slv_sntn@yahoo.com>
2022-05-07 15:28:25 +0300

    first commit

--- src/main/java/com/example/onlinehotelbookingsystem/web/HotelRestController.java
+++ src/main/java/com/example/onlinehotelbookingsystem/web/HotelRestController.java
@@ -4,14 +4,13 @@ import com.example.onlinehotelbookingsystem.model.view.AccommodationViewModel;
 import com.example.onlinehotelbookingsystem.service.AccommodationService;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@CrossOrigin(origins = "*")
+//@CrossOrigin(origins = "*")
 @RestController
 public class HotelRestController {
 
