5b392dd49b45121e228499fc2a16fa890de95b93
Santanasj <slv_sntn@yahoo.com>
2022-05-22 14:15:20 +0300

    first commit

--- src/main/resources/static/js/search.js
+++ src/main/resources/static/js/search.js
@@ -82,46 +82,7 @@ const outputHTML = matches => {
 
         document.getElementById('output').innerHTML = output
 
-    }
-
-        //     matches.map(match =>
-        //     `<div class="hotel py-4 px-2 pb-4 border-bottom">
-        //   <div class="row">
-        //     <div class="col-lg-3 px-4">
-        //       <img src= ${match.imageUrl}
-        //            height="200" width="250" alt="" class="hotel-img rounded">
-        //     </div>
-        //     <div class="col-lg-3">
-        //       <div class="d-md-flex align-items-md-center">
-        //         <div class="fw-bold">${match.name}</div>
-        //       </div>
-        //       <div class="rating">Category: ${match.category}</div>
-        //       <div class="row">
-        //
-        //         <div class="col fw-light fs-6">${match.address}, ${match.postalCode}</div>
-        //       </div>
-        //
-        //       <div class="row">
-        //         <div class="col fw-light fs-6">${match.city}</div>
-        //       </div>
-        //       <div class="row">
-        //         <div class="col fw-light fs-6">${match.type}</div>
-        //       </div>
-        //     </div>
-        //     <div class="col-lg-3">
-        //       Description
-        //     </div>
-        //     <div class="col-lg-3 px-0">
-        //       <iframe class="mb-4 mb-lg-0 rounded" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d12097.433213460943!2d-74.0062269!3d40.7101282!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xb89d1fe6bc499443!2sDowntown+Conference+Center!5e0!3m2!1smk!2sbg!4v1539943755621" frameborder="0" style="border:0; width: 90%; height: 200px;" allowfullscreen></iframe>
-        //     </div>
-        //   </div>
-        //   <!--                                        Book Now button -->
-        //   <div class="d-flex justify-content-end mt-4 px-4">
-        //     <button class="btn btn-primary booking-btn" data-hotel-id='${match.id}'>Book Now</button>
-        //   </div>
-        // </div>`).join('');
-    // output.innerHTML = html;
-    else {
+    } else {
         const msg = `<h4>No Matches Found!</h4>`;
         output.innerHTML = msg;
     }
