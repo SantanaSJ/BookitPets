30d168f7ddbc64ae8f9af68fddfb40d1c17c073b
Santanasj <slv_sntn@yahoo.com>
2022-08-17 16:28:15 +0300

    minor changes

--- src/main/java/com/example/onlinehotelbookingsystem/scheduler/SetBookingAsComplete.java
+++ src/main/java/com/example/onlinehotelbookingsystem/scheduler/SetBookingAsComplete.java
@@ -21,8 +21,7 @@ public class SetBookingAsComplete {
 
     //    @Scheduled(cron = "* 0 22 * * 7")
 //    sec min hour
-//    @Scheduled(cron = "00 05 12 * * *")
-    @Scheduled(cron = "00 45 15 * * *")
+    @Scheduled(cron = "00 05 12 * * *")
     private void moveCompletedBookings() {
         this.bookingService.setBookingAsComplete();
         LOGGER.info("Set successfully at {}", LocalDateTime.now());
