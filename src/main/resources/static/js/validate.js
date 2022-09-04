4c6c4a58da268bb250bfbff2aeffc3d1311a6f09
Santanasj <slv_sntn@yahoo.com>
2022-09-04 19:56:28 +0300

    remember me functionality added

---
+++ src/main/resources/static/js/validate.js
@@ -0,0 +1,13 @@
+// $('#submit').click(function () {
+//     const select = document.getElementById("numberOfRooms");
+//     console.log(select.value);
+//     if (select.value === "0") {
+//         let paragraphElement = document.createElement("p");
+//         paragraphElement.innerHTML = "Please select at least one room!";
+//         document.getElementById("myDiv").appendChild(paragraphElement);
+//     }
+// })
+$('#submit').click(function () {
+
+})
+
